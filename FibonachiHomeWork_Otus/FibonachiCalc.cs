71973e11810b05834a203c2cbeecad7b30020366
Ilya Shadrin <90275369+dekey111@users.noreply.github.com>
2025-05-05 18:18:51 +0300

    Вуп-вуп

---
+++ FibonachiHomeWork_Otus/FibonachiCalc.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonachiHomeWork_Otus
+{
+    class FibonachiCalc
+    {
+        /// <summary>
+        /// Рекурсивный метод для вычисления числа Фибоначчи
+        /// </summary>
+        /// <param name="n">Принимает значение фибоначчи</param>
+        /// <returns>Возвращает число фибоначчи</returns>
+        public int FibonacciRecursive(int n)
+        {
+            if (n <= 1)
+                return n;
+            return FibonacciRecursive(n - 1) + FibonacciRecursive(n - 2);
+        }
+
+        /// <summary>
+        /// Итеративный метод для вычисления числа Фибоначчи
+        /// </summary>
+        /// <param name="n">Принимает значение фибоначчи</param>
+        /// <returns>Возврвщает число фибоначчи</returns>
+        public int FibonacciIterative(int n)
+        {
+            if (n <= 1)
+                return n;
+
+            int a = 0, b = 1, temp;
+
+            for (int i = 2; i <= n; i++)
+            {
+                temp = a + b;
+                a = b;
+                b = temp;
+            }
+
+            return b;
+        }
+
+        /// <summary>
+        /// Метод для измерения времени выполнения функциции
+        /// </summary>
+        /// <param name="action"></param>
+        /// <returns>Возврашает время выполнения</returns>
+        public TimeSpan MeasureExecutionTime(Func<int> action)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            action();
+            stopwatch.Stop();
+            return stopwatch.Elapsed;
+        }
+    }
+}
---
+++ FibonachiHomeWork_Otus/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FibonachiHomeWork_Otus;
+
+Console.WriteLine("Hello, World!");
+
+
+var calculator = new FibonachiCalc();
+
+int[] valuesToTest = { 5, 10, 20 };
+
+foreach (var n in valuesToTest)
+{
+    Console.WriteLine($"\nВычисление F({n}):");
+
+    // Измерение рекурсивного метода
+    var recursiveTime = calculator.MeasureExecutionTime(() => calculator.FibonacciRecursive(n));
+    int resultRecursive = calculator.FibonacciRecursive(n);
+    Console.WriteLine($"Рекурсивный метод: F({n}) = {resultRecursive}, время = {recursiveTime.TotalMilliseconds:F6} мс");
+
+    // Измерение итеративного метода
+    var iterativeTime = calculator.MeasureExecutionTime(() => calculator.FibonacciIterative(n));
+    int resultIterative = calculator.FibonacciIterative(n);
+    Console.WriteLine($"Итеративный метод: F({n}) = {resultIterative}, время = {iterativeTime.TotalMilliseconds:F6} мс");
+}
